ca836154b6bf57ef28c857d7cdc38bb2f6df87ec
Jairo Cesar Curia <102192577+JCCuria17@users.noreply.github.com>
2022-11-08 17:45:14 -0300

    Teste de caracteres
    Teste de variáveis do tipo char, string e concatenação de variáveis. O tipo char só imprime letras e mesmo atribuido valores numéricos ele busca na tabela a letra correspondente e faz a impressão.

---
+++ testa-caracteres/src/testaCaracteres/TestaCaracteres.java
@@ -0,0 +1,25 @@
+package testaCaracteres;
+
+public class TestaCaracteres {
+
+	public static void main(String[] args) {
+
+        char letra = 'a';
+        System.out.println(letra);
+
+        char valor = 65;  
+        System.out.println(valor);
+        
+        valor = (char) (valor + 1); 
+        System.out.println(valor);
+        
+        String palavra = "Teste Variavel String";
+        System.out.println(palavra);
+
+        palavra = palavra + 2020;
+        System.out.println(palavra);
+
+
+	}
+
+}
